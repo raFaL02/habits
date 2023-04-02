29e7642df2cba7f33f68f3e1bd0f3b942ce54c21
raFa <rafalimabcl@gmail.com>
2023-04-02 12:54:00 -0300

    initial commit

---
+++ web/web/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+
+import { Habit } from "./components/Habit"
+
+export function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={10} />
+      <Habit completed={20} />
+      <Habit completed={30} />
+    </div>
+  )
+}
